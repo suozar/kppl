5535bb932e5dbbeebef9946e0078a0150abd2a8c
suozar <errymadhan@gmail.com>
2015-04-01 18:11:29 +0700

    Create bagiankiri.php

---
+++ bagiankiri.php
@@ -0,0 +1,16 @@
+<div class="col-md-12" style="background-color: #ccc"><!--BAGIAN MENU KIRI-->
+    <br>
+    <div class="list-group">
+        <a href="pesan.php" class="list-group-item">
+            <span class="glyphicon glyphicon-envelope"></span> Pesan <span class="badge">0</span>
+        </a>
+ 
+        <a href="galeri.php" class="list-group-item">
+            <span class="glyphicon glyphicon-picture"></span> Galeri <span class="badge">0</span>
+        </a>
+ 
+        <a href="teman.php" class="list-group-item">
+            <span class="glyphicon glyphicon-user"></span> Teman <span class="badge">0</span>
+        </a>
+    </div>
+</div><!--/.BAGIAN MENU KIRI-->
